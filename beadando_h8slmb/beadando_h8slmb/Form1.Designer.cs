f1e9e0ab074e4b381f6dc60cb5ce9c8e0e7ef731
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-12-11 12:04:38 +0100

    Xml beolvasása

--- beadando_h8slmb/beadando_h8slmb/Form1.Designer.cs
+++ beadando_h8slmb/beadando_h8slmb/Form1.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.searchText = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchList = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // searchText
+            // 
+            this.searchText.Location = new System.Drawing.Point(13, 13);
+            this.searchText.Name = "searchText";
+            this.searchText.Size = new System.Drawing.Size(137, 20);
+            this.searchText.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(156, 10);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // searchList
+            // 
+            this.searchList.HideSelection = false;
+            this.searchList.HoverSelection = true;
+            this.searchList.Location = new System.Drawing.Point(13, 57);
+            this.searchList.Name = "searchList";
+            this.searchList.Size = new System.Drawing.Size(218, 129);
+            this.searchList.TabIndex = 2;
+            this.searchList.UseCompatibleStateImageBehavior = false;
+            this.searchList.View = System.Windows.Forms.View.List;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(940, 507);
+            this.Controls.Add(this.searchList);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchText);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox searchText;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ListView searchList;
     }
 }
 
--- beadando_h8slmb/beadando_h8slmb/Form1.cs
+++ beadando_h8slmb/beadando_h8slmb/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace beadando_h8slmb
 {
@@ -15,6 +16,27 @@ namespace beadando_h8slmb
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            searchList.Clear();
+            if (searchText.Text  != null)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load("subscriptions.xml");
+
+                foreach (XmlNode node in doc.DocumentElement)
+                {
+                    string name = node.Attributes[0].InnerText;
+                    if (name == searchText.Text)
+                    {
+                        foreach (XmlNode child in node.ChildNodes)
+                        {
+                            searchList.Items.Add(child.InnerText);
+                        }
+                    }
+                }
+            }
         }
     }
 }
